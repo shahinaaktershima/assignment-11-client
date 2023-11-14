a4ba18723dbdbf34a51ae6b973449e53f469a91b
shahinaaktershima <shima421914@gmail.com>
2023-11-14 22:57:33 +0600

    data-load

--- src/component/Home/Jobs.jsx
+++ src/component/Home/Jobs.jsx
@@ -6,36 +6,46 @@ import Card from "./Card";
 
 const Jobs = () => {
     const [jobs,setJobs]=useState([]);
+    const [displayJobs,setDisplay]=useState([])
     useEffect(()=>{
          fetch('https://job-project-server.vercel.app/jobs')
          .then(res=>res.json())
-         .then(data=>setJobs(data))
+         .then(data=>{
+            setJobs(data);
+            setDisplay(data)
+         })
     },[])
     const handleJobs=(jobtype)=>{
-       
+       if(!jobtype){
+        setDisplay([...jobs])
+        return 
+       }
             const filteredJobs=jobs.filter(job=>job.type===jobtype)
-            setJobs(filteredJobs)
+            setDisplay(filteredJobs)
         }
     return (
         <div className="my-6">
             
             <Tabs className='space-y-3'>
         <TabList className='grid grid-cols-5   gap-5'>
-          <Tab className='btn'>All Jobs</Tab>
+          <Tab className='btn' onClick={()=>handleJobs()}>All Jobs</Tab>
           <Tab className='btn' onClick={() => handleJobs("Remote Job")}>remote job</Tab>
           <Tab className='btn' onClick={() => handleJobs("Hybrid")}>Hybrid Jobs</Tab>
           <Tab className='btn' onClick={() => handleJobs("Onsite Job")}>Onsite Jobs</Tab>
           <Tab className='btn' onClick={() => handleJobs("Part Time")}>Part-Time Jobs</Tab>
           </TabList>
-          <TabPanel className='grid lg:grid-cols-4 md:grid-cols-3 grid-cols-1 gap-5'>
+          {/* <TabPanel > */}
        
+       <div className='grid lg:grid-cols-4 md:grid-cols-3 grid-cols-1 gap-5'>
         
        {
-           jobs.map(job=><Card key={job._id} job={job} ></Card>)
+           displayJobs.map(job=><Card key={job._id} job={job} ></Card>)
        }
 
-      </TabPanel>
-      <TabPanel className='grid lg:grid-cols-4 md:grid-cols-3 grid-cols-1 gap-5 '>
+        </div> 
+      
+      {/* </TabPanel> */}
+      {/* <TabPanel className='grid lg:grid-cols-4 md:grid-cols-3 grid-cols-1 gap-5 '>
         
         {
             jobs.map(job=><Card key={job._id} job={job} ></Card>)
@@ -59,7 +69,7 @@ const Jobs = () => {
          {
             jobs.map(job=><Card key={job._id} job={job} ></Card>)
         }
-       </TabPanel>
+       </TabPanel> */}
           </Tabs>
         </div>
     );
